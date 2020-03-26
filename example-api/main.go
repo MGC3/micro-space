14b630a9b96fa91a3836decdc142872f62011223
michaelchun <michael.chun03@gmail.com>
2020-03-26 01:19:19 -0400

    Use custom server instead of default

--- example-api/main.go
+++ example-api/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -19,7 +20,16 @@ func main() {
 		log.Fatal("Error setting port")
 	}
 
+	// create custom server, for more control
+	srv := http.Server{
+		Addr:         ":" + port,
+		Handler:      r,
+		ReadTimeout:  1 * time.Second,   // max duration for reading client req - make higher if client uploading big file, etc.
+		WriteTimeout: 1 * time.Second,   // max duration for writing to client - small is fine for sending basic JSON
+		IdleTimeout:  120 * time.Second, // all around connection pooling - allows client to resuse the same connection, useful if they have multiple requests. Particularly useful for microservices
+	}
+
 	// start server
 	fmt.Println("Server listening at:", port)
-	log.Fatal(http.ListenAndServe(":"+port, r))
+	log.Fatal(srv.ListenAndServe())
 }
