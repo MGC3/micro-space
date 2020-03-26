ba52f9a69529e1d86a386c3fecb277ece88e31f2
michaelchun <michael.chun03@gmail.com>
2020-03-26 01:07:02 -0400

    Add gorilla mux

--- example-api/main.go
+++ example-api/main.go
@@ -5,14 +5,21 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
+	// create router
+	r := mux.NewRouter()
+
+	// grab port from env
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("Error setting port")
 	}
 
+	// start server
 	fmt.Println("Server listening at:", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
